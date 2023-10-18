ace6a8da2cfc95f454e4838bfc8f7f2e9ee1799e
Keshav-Singhal5 <107481366+Keshav-Singhal5@users.noreply.github.com>
2023-10-18 22:23:54 +0530

    Keshav portfolio website
    hey this is committed

---
+++ script.js
@@ -0,0 +1,10 @@
+// window.addEventListener("scroll", function () {
+//     const box2 = document.querySelector(".box2[data-type='background']");
+//     const box2Offset = box2.offsetTop;
+  
+//     if (window.scrollY >= box2Offset) {
+//       const offset = (window.scrollY - box2Offset) * 0.5;
+//       box2.style.transform = `translateY(calc(100% + ${offset}px))`;
+//     }
+//   });
+  
